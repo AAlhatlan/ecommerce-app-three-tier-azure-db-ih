7c5889fed27ebeb1a930b854381b1b706544a334
AAlhatlan <ahmadalhatlan@gmail.com>
2025-09-11 02:04:29 +0300

    Update api.ts

--- ecommerce-app-frontend/src/services/api.ts
+++ ecommerce-app-frontend/src/services/api.ts
@@ -12,7 +12,7 @@ import {
   CreateOrderData 
 } from '../types';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://158.158.104.245:3001/api';
 
 // Create axios instance
 const api = axios.create({
